5590a88ce8bd9500e386dcafa4428cb0733ab911
Cecilia Gouveia <cecilia_gouveia@hotmail.com>
2025-09-05 14:26:38 +0100

    implement navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,29 @@
-import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Bricolage_Grotesque } from "next/font/google"
+import "./globals.css"
+import Navbar from "@/components/layout/navbar/navbar"
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Converso",
   description: "Real-time AI Teaching Platform",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
+      <body className={`${bricolage.variable} antialiased`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
